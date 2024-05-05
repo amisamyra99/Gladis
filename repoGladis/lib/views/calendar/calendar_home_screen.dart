c3553c4a658fc72cadb25bb9dc915b4e897a0b1a
Ami Ouedraogo <AOuedraogo@intershop.com>
2024-05-05 18:25:25 +0200

    feature : calendar main screen with all type of views

---
+++ repoGladis/lib/views/calendar/calendar_home_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+class CalendarScreen extends StatelessWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final CalendarController _controller = CalendarController();
+    Color? _headerColor, _viewHeaderColor, _calendarColor;
+    var size=MediaQuery.of(context).size;
+    return  Scaffold(
+      body: SafeArea(
+        child:
+        SfCalendar(
+          view: CalendarView.day,
+          allowedViews: const [
+            CalendarView.day,
+            CalendarView.week,
+            CalendarView.workWeek,
+            CalendarView.month,
+            CalendarView.schedule,
+
+          ],
+          initialDisplayDate: DateTime.now(),
+          showDatePickerButton: true,
+          monthViewSettings: const MonthViewSettings(
+            showTrailingAndLeadingDates: true,
+          ),
+
+      ),
+      ),
+
+
+    );
+  }
+}
